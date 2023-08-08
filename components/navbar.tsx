e50460c5e11158c01b49b220eede1c0941d5db8d
w-dib <wdanieldib@gmail.com>
2023-08-08 17:35:48 +0400

    Fix css, fix navbar.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,17 +2,36 @@ import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import { ModeToggle } from "./mode-toggle";
 import Link from "next/link";
+import { Menu } from "lucide-react";
+import { Poppins } from "next/font/google";
+
+const font = Poppins({
+  weight: "600",
+  subsets: ["latin"],
+});
 
 function Navbar() {
   return (
-    <header className="w-full fixed z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
-      <div>
-        <Link href="/">
-          <Image src="/logo.svg" alt="logo" width={32} height={32} />
+    <header className="w-full fixed z-40 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+      <div className="flex items-center">
+        <Menu className="md:hidden block" />
+        <Link href="/" className="hidden md:flex items-center">
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            width={32}
+            height={32}
+            className="mr-2"
+          />
+          <h1
+            className={`text-xl md:text-3xl font-bold text-primary ${font.className} bg-gradient-to-r from-blue-500 to-sky-600 bg-clip-text text-transparent`}
+          >
+            Loula
+          </h1>
         </Link>
       </div>
-      <div>
-        {/* <ModeToggle /> */}
+      <div className="flex justify-between items-center z-50">
+        <ModeToggle />
         <UserButton afterSignOutUrl="/" />
       </div>
     </header>
