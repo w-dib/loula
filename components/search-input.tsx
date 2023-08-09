bc378e45b46dfe0ce89510b9cfe12f19091a0a24
w-dib <wdanieldib@gmail.com>
2023-08-09 22:24:35 +0400

    Start search input

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -2,8 +2,14 @@
 
 import { Search } from "lucide-react";
 import { Input } from "./ui/input";
+import { useRouter, useSearchParams } from "next/navigation";
 
 function SearchInput() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const categoryId = searchParams.get("categoryId");
+
   return (
     <div className="relative">
       <Search className="absolute h-4 w-4 top-3 left-4 text-muted-foreground" />
