06d6dbdda3924f3237aa4117b0b6eda324e12877
w-dib <wdanieldib@gmail.com>
2023-08-21 12:24:37 +0400

    Only show submissions by same user

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,4 +1,6 @@
 import prismadb from "@/lib/prismadb";
+import { auth, redirectToSignIn } from "@clerk/nextjs";
+
 import Categories from "@/components/categories";
 import ExpenseCard from "@/components/expense-card";
 import SearchInput from "@/components/search-input";
@@ -6,8 +8,17 @@ import { Separator } from "@/components/ui/separator";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default async function Home() {
+  const { userId } = auth();
+  if (!userId) {
+    return redirectToSignIn();
+  }
+
   const categories = await prismadb.category.findMany();
-  const expenses = await prismadb.expense.findMany();
+  const expenses = await prismadb.expense.findMany({
+    where: {
+      userId,
+    },
+  });
 
   const categoryMap: Record<string, string> = {};
   categories.forEach((category) => {
@@ -16,21 +27,23 @@ export default async function Home() {
 
   return (
     <main className="h-full p-4 space-y-2">
-      <Tabs defaultValue="expenses" className="w-full">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="expenses">Expenses</TabsTrigger>
-          <TabsTrigger value="budget">Budget</TabsTrigger>
-        </TabsList>
-        <TabsContent value="expenses">
-          <div>
-            <SearchInput />
-            <Categories data={categories} />
-            <Separator />
-            <ExpenseCard data={expenses} categoryMap={categoryMap} />
-          </div>
-        </TabsContent>
-        <TabsContent value="budget">Change your budget here.</TabsContent>
-      </Tabs>
+      <div>
+        <Tabs defaultValue="expenses" className="w-full">
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="expenses">Expenses</TabsTrigger>
+            <TabsTrigger value="budget">Budget</TabsTrigger>
+          </TabsList>
+          <TabsContent value="expenses">
+            <div>
+              <SearchInput />
+              <Categories data={categories} />
+              <Separator />
+              <ExpenseCard data={expenses} categoryMap={categoryMap} />
+            </div>
+          </TabsContent>
+          <TabsContent value="budget">Change your budget here.</TabsContent>
+        </Tabs>
+      </div>
     </main>
   );
 }
