2c8b9b6e8d8e5478f2c01d3497d22f106382c90a
w-dib <wdanieldib@gmail.com>
2023-08-21 12:36:17 +0400

    Add expense quick button for mobile

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,11 +1,13 @@
 import prismadb from "@/lib/prismadb";
 import { auth, redirectToSignIn } from "@clerk/nextjs";
+import Link from "next/link";
 
 import Categories from "@/components/categories";
 import ExpenseCard from "@/components/expense-card";
 import SearchInput from "@/components/search-input";
 import { Separator } from "@/components/ui/separator";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Plus } from "lucide-react";
 
 export default async function Home() {
   const { userId } = auth();
@@ -25,6 +27,11 @@ export default async function Home() {
     categoryMap[category.id] = category.name;
   });
 
+  const totalAmount = expenses.reduce(
+    (sum, expense) => sum + expense.amount,
+    0
+  );
+
   return (
     <main className="h-full p-4 space-y-2">
       <div>
@@ -43,6 +50,19 @@ export default async function Home() {
           </TabsContent>
           <TabsContent value="budget">Change your budget here.</TabsContent>
         </Tabs>
+        <Link href="/expense/new">
+          <button className="fixed bottom-20 right-2 rounded-full bg-primary text-white p-4 shadow-lg block sm:hidden hover:opacity-75">
+            <Plus />
+          </button>
+        </Link>
+        <div className="fixed bottom-0 left-0 right-0 bg-white p-4 border-t shadow-lg">
+          <p className="text-primary text-lg text-center">
+            <span className="text-primary font-bold text-lg">
+              Total Amount: {""}
+            </span>
+            {totalAmount} AED
+          </p>
+        </div>
       </div>
     </main>
   );
