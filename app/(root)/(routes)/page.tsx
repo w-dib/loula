15599ac16b60270a00aac38fce20de3d9350370f
w-dib <wdanieldib@gmail.com>
2023-08-21 12:00:50 +0400

    map expenses on main page

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,18 +1,36 @@
+import prismadb from "@/lib/prismadb";
 import Categories from "@/components/categories";
 import ExpenseCard from "@/components/expense-card";
 import SearchInput from "@/components/search-input";
 import { Separator } from "@/components/ui/separator";
-import prismadb from "@/lib/prismadb";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default async function Home() {
   const categories = await prismadb.category.findMany();
+  const expenses = await prismadb.expense.findMany();
+
+  const categoryMap: Record<string, string> = {};
+  categories.forEach((category) => {
+    categoryMap[category.id] = category.name;
+  });
 
   return (
     <main className="h-full p-4 space-y-2">
-      <SearchInput />
-      <Categories data={categories} />
-      <Separator />
-      <ExpenseCard />
+      <Tabs defaultValue="expenses" className="w-full">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="expenses">Expenses</TabsTrigger>
+          <TabsTrigger value="budget">Budget</TabsTrigger>
+        </TabsList>
+        <TabsContent value="expenses">
+          <div>
+            <SearchInput />
+            <Categories data={categories} />
+            <Separator />
+            <ExpenseCard data={expenses} categoryMap={categoryMap} />
+          </div>
+        </TabsContent>
+        <TabsContent value="budget">Change your budget here.</TabsContent>
+      </Tabs>
     </main>
   );
 }
--- components/expense-card.tsx
+++ components/expense-card.tsx
@@ -1,3 +1,5 @@
+import { Expense } from "@prisma/client";
+
 import {
   Card,
   CardContent,
@@ -9,30 +11,38 @@ import {
 import { cn } from "@/lib/utils";
 import { Pencil, Trash } from "lucide-react";
 
-function ExpenseCard() {
+interface ExpenseCardProps {
+  data: Expense[];
+  categoryMap: Record<string, string>;
+}
+
+function ExpenseCard({ data, categoryMap }: ExpenseCardProps) {
+  console.log(categoryMap); // Log the categoryMap to the console
+
   return (
-    <div>
-      <Card>
-        <CardHeader>
-          <CardTitle>
-            <div className="flex justify-between items-center">
-              <div>
-                <p className="text-primary">Long text Long text</p>
-              </div>
-              <div className="flex space-x-2">
-                <div className="text-muted-foreground text-xs group flex p-3 w-full justify-start cursor-pointer hover:text-primary hover:bg-primary/10 rounded-xl transition">
-                  <Pencil className="w-5 h-5" />
+    <div className="mt-3 flex flex-col lg:flex-row gap-2">
+      {data.map((item) => (
+        <Card key={item.id}>
+          <CardHeader>
+            <CardTitle>
+              <div className="flex justify-between items-center">
+                <div>
+                  <p className="text-primary">{item.name}</p>
                 </div>
-                <div className="text-muted-foreground text-xs group flex p-3 w-full justify-start cursor-pointer hover:text-destructive hover:bg-destructive/10 rounded-xl transition">
-                  <Trash className="w-5 h-5" />
+                <div className="flex space-x-2">
+                  <div className="text-muted-foreground text-xs group flex p-3 w-full justify-start cursor-pointer hover:text-primary hover:bg-primary/10 rounded-xl transition">
+                    <Pencil className="w-5 h-5" />
+                  </div>
+                  <div className="text-muted-foreground text-xs group flex p-3 w-full justify-start cursor-pointer hover:text-destructive hover:bg-destructive/10 rounded-xl transition">
+                    <Trash className="w-5 h-5" />
+                  </div>
                 </div>
               </div>
-            </div>
-          </CardTitle>
-          <CardDescription>
-            <div
-              className={cn(
-                `
+            </CardTitle>
+            <CardDescription>
+              <div
+                className={cn(
+                  `
       flex
       items-center
       text-cener
@@ -48,21 +58,22 @@ function ExpenseCard() {
       transition
       w-min
       `
-              )}
-            >
-              Transportation
+                )}
+              >
+                {categoryMap[item.categoryId]}
+              </div>
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <p className="text-primary text-lg">{item.amount} AED</p>
+          </CardContent>
+          <CardFooter className="text-xs flex justify-between">
+            <div>
+              <p>{item.date.toString()}</p>
             </div>
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p className="text-primary">230 AED</p>
-        </CardContent>
-        <CardFooter className="text-xs flex justify-between">
-          <div>
-            <p>18th August 2023</p>
-          </div>
-        </CardFooter>
-      </Card>
+          </CardFooter>
+        </Card>
+      ))}
     </div>
   );
 }
