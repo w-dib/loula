fbe814f4f87cfba5346586039c0abc24e7a765b6
w-dib <wdanieldib@gmail.com>
2023-08-22 11:34:12 +0400

    change fixed to sticky

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -55,7 +55,7 @@ export default async function Home() {
             <Plus />
           </button>
         </Link>
-        <div className="fixed bottom-0 left-0 right-0 bg-white p-4 border-t shadow-lg">
+        <div className="sticky bottom-0 left-0 right-0 bg-white p-4 border-t shadow-lg">
           <p className="text-primary text-lg text-center">
             <span className="text-primary font-bold text-lg">
               Total Amount: {""}
